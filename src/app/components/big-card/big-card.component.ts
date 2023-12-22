830cc6ee944b586eee0bbdb90ec775d7bb37a661
joaosantosmedeiros <jopesame@gmail.com>
2023-12-22 07:01:50 -0300

    feat: add components dynamics properties

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
-
+  @Input()
+  photoCover: string = 'https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png';
+  @Input()
+  cardDescription: string = 'Default Description';
+  @Input()
+  title: string = 'Default Title';
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
   imports: [],
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+  styleUrl: './small-card.component.css',
 })
 export class SmallCardComponent {
+  @Input()
+  title: any = 'Default title';
 
+  @Input()
+  photoCover: any = 'https://i0.wp.com/ouvidoria.jaboatao.pe.gov.br/wp-content/uploads/2023/10/placeholder-23-1.png?fit=1200%2C800&ssl=1';
 }
